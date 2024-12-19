3611804ab78a5b3c6ef14949ed40cb7e407f88d3
smazik02 <s.mazik2002@gmail.com>
2024-12-19 14:08:37 +0100

    Day 15 part 2

--- Day15/src/Main.kt
+++ Day15/src/Main.kt
@@ -28,13 +28,102 @@ fun move(instruction: Direction, currentPoint: Point): Point {
             }
         }
 
-        else -> return currentPoint
+        else -> throw UnsupportedOperationException()
+    }
+}
+
+fun checkIfCanMove(instruction: Direction, currentPoint: Point): Point {
+    val nextPoint = when (instruction) {
+        Direction.UP -> Point(currentPoint.x - 1, currentPoint.y)
+        Direction.DOWN -> Point(currentPoint.x + 1, currentPoint.y)
+        Direction.LEFT -> Point(currentPoint.x, currentPoint.y - 1)
+        Direction.RIGHT -> Point(currentPoint.x, currentPoint.y + 1)
+    }
+
+    when (mapBoard[nextPoint.x][nextPoint.y]) {
+        '#' -> return currentPoint
+        '.' -> return nextPoint
+        '[', ']' -> {
+            when (instruction) {
+                Direction.LEFT, Direction.RIGHT -> {
+                    val newPoint = checkIfCanMove(instruction, nextPoint)
+                    return if (newPoint != nextPoint) {
+                        nextPoint
+                    } else {
+                        currentPoint
+                    }
+                }
+
+                Direction.UP, Direction.DOWN -> {
+                    val newPoint = checkIfCanMove(instruction, nextPoint)
+                    val otherNextPoint =
+                        if (mapBoard[nextPoint.x][nextPoint.y] == '[')
+                            Point(nextPoint.x, nextPoint.y + 1)
+                        else
+                            Point(nextPoint.x, nextPoint.y - 1)
+                    val otherNewPoint = checkIfCanMove(instruction, otherNextPoint)
+
+                    return if (newPoint != nextPoint && otherNewPoint != otherNextPoint) {
+                        nextPoint
+                    } else {
+                        currentPoint
+                    }
+                }
+            }
+        }
+
+        else -> throw UnsupportedOperationException()
+    }
+}
+
+fun move2(instruction: Direction, currentPoint: Point): Point {
+    val nextPoint = when (instruction) {
+        Direction.UP -> Point(currentPoint.x - 1, currentPoint.y)
+        Direction.DOWN -> Point(currentPoint.x + 1, currentPoint.y)
+        Direction.LEFT -> Point(currentPoint.x, currentPoint.y - 1)
+        Direction.RIGHT -> Point(currentPoint.x, currentPoint.y + 1)
+    }
+
+    when (mapBoard[nextPoint.x][nextPoint.y]) {
+        '#' -> return currentPoint
+        '.' -> return nextPoint
+        '[', ']' -> {
+            when (instruction) {
+                Direction.LEFT, Direction.RIGHT -> {
+                    val newPoint = move2(instruction, nextPoint)
+                    mapBoard[newPoint.x][newPoint.y] = mapBoard[nextPoint.x][nextPoint.y]
+                    mapBoard[nextPoint.x][nextPoint.y] = '.'
+                    return nextPoint
+                }
+
+                Direction.UP, Direction.DOWN -> {
+                    val newPoint = move2(instruction, nextPoint)
+                    val otherNextPoint =
+                        if (mapBoard[nextPoint.x][nextPoint.y] == '[')
+                            Point(nextPoint.x, nextPoint.y + 1)
+                        else
+                            Point(nextPoint.x, nextPoint.y - 1)
+                    val otherNewPoint = move2(instruction, otherNextPoint)
+
+                    mapBoard[newPoint.x][newPoint.y] = mapBoard[nextPoint.x][nextPoint.y]
+                    mapBoard[otherNewPoint.x][otherNewPoint.y] = mapBoard[otherNextPoint.x][otherNextPoint.y]
+                    mapBoard[nextPoint.x][nextPoint.y] = '.'
+                    mapBoard[otherNextPoint.x][otherNextPoint.y] = '.'
+
+                    return nextPoint
+                }
+            }
+        }
+
+        else -> throw UnsupportedOperationException()
     }
 }
 
 fun main() {
     val instructionSet = mutableListOf<Direction>()
     var robotPos = Point(0, 0)
+
+    // PART 1
 
     val fileContent = File("inputs/input.txt").readLines().map { it.trim() }
     fileContent.forEachIndexed { idx, line ->
@@ -69,9 +158,6 @@ fun main() {
         }
     }
 
-//    mapBoard[robotPos.x][robotPos.y] = '@'
-//    mapBoard.forEach { println(it) }
-
     var coordSum = 0
     mapBoard.forEachIndexed { rowIdx, row ->
         row.forEachIndexed rowLoop@{ colIdx, place ->
@@ -82,4 +168,47 @@ fun main() {
     }
 
     println("Total sum: $coordSum")
+
+    // PART 2
+    mapBoard.clear()
+
+    fileContent.forEachIndexed { rowIdx, line ->
+        if (line.isEmpty())
+            return@forEachIndexed
+
+        if (line.first() == '#') {
+            val rowToAdd = mutableListOf<Char>()
+            line.forEachIndexed { colIdx, char ->
+                when (char) {
+                    '#' -> rowToAdd.addAll("##".toList())
+                    'O' -> rowToAdd.addAll("[]".toList())
+                    '.' -> rowToAdd.addAll("..".toList())
+                    '@' -> {
+                        robotPos = Point(rowIdx, colIdx * 2)
+                        rowToAdd.addAll("..".toList())
+                    }
+                }
+            }
+            mapBoard.add(rowToAdd)
+        }
+    }
+
+    for (instruction in instructionSet) {
+        val newPoint = checkIfCanMove(instruction, robotPos)
+        if (newPoint != robotPos) {
+            move2(instruction, robotPos)
+            robotPos = newPoint
+        }
+    }
+
+    coordSum = 0
+    mapBoard.forEachIndexed { rowIdx, row ->
+        row.forEachIndexed rowLoop@{ colIdx, place ->
+            if (place != '[') return@rowLoop
+
+            coordSum += (100 * rowIdx + colIdx)
+        }
+    }
+
+    println("Total sum part 2: $coordSum")
 }
